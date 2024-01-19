10861c1d98b137f51586bbf26a51c1caea3c6d53
Yihun Alemayehu <yankure01@gmail.com>
2024-01-19 16:23:17 +0300

    Special commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -7,8 +8,23 @@ void main() {
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // late MapController controller;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   controller = MapController(
+  //     initMapWithUserPosition: const UserTrackingOption(),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +33,39 @@ class HomePage extends StatelessWidget {
         title: const Text('LocaPick'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+          // child: OSMFlutter(
+          //   controller: controller,
+          //   osmOption: OSMOption(
+          //     isPicker: true,
+          //     // userLocationMarker: UserLocationMaker(personMarker: personMarker, directionArrowMarker: directionArrowMarker),
+          //     markerOption: MarkerOption(
+          //       advancedPickerMarker: const MarkerIcon(
+          //         icon: Icon(
+          //           Icons.pin_drop,
+          //           color: Colors.black,
+          //           size: 300,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          var point = await showSimplePickerLocation(
+              context: context,
+              isDismissible: true,
+              title: 'Pcik Location',
+              textConfirmPicker: 'Pick',
+              textCancelPicker: 'Cancel',
+              // initCurrentUserPosition: UserTrackingOption(),
+              initPosition: GeoPoint(latitude: 47.43, longitude: 8.47));
+
+          if (point != null) {
+            print('${point.latitude} , ${point.longitude}');
+          }
+        },
         child: const Icon(Icons.pin_drop),
       ),
     );
